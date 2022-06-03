3abd0f900bea33981a634161cb73bff1164fa3d1
kaiolabs <peypro1@gmail.com>
2022-06-02 22:49:23 -0300

    Update

--- src/components/routers/routes.js
+++ src/components/routers/routes.js
@@ -24,13 +24,9 @@ const Rotas = () => (
         <Routes>
             <Route exact path="/" element={<LoginPage/>}></Route>
             <Route path='*' element={<h1>Not Found</h1>}/>
-
-
-            <Route path="/6f92b345-0ea3-4d40-8791-2512b476e875" element={
-                <PrivateRoute>
-                   <EscolhaDeCadastro/>
-                </PrivateRoute>
-            }/>
+            <Route path="/6f92b345-0ea3-4d40-8791-2512b476e875" element={<EscolhaDeCadastro/>}/>
+            <Route path="/c8188d50-7e7e-44d5-81fa-6d666563eff3" element={<CadastraONG/>}/>
+            <Route path="/9018cb68-66e3-4fdd-bf90-bfb54d52b0f7" element={<CadastraProtetorIndependente/>}/>
             
 
             <Route path="/5872b189-7ede-4e1d-895e-45a966e4c876" element={
@@ -53,9 +49,6 @@ const Rotas = () => (
                 </PrivateRoute>
             }/>
 
-            <Route path="/c8188d50-7e7e-44d5-81fa-6d666563eff3" element={<CadastraONG/>}/>
-
-            <Route path="/9018cb68-66e3-4fdd-bf90-bfb54d52b0f7" element={<CadastraProtetorIndependente/>}/>
 
             <Route path="/e6851794-1cff-4aea-a1b6-5686a6f2afb6" element={
                 <PrivateRoute>
