5bae112fdd06d052f31c2a5147aa873458585c90
kaiolabs <peypro1@gmail.com>
2022-06-05 23:30:44 -0300

    Update

--- src/components/server/supabase.js
+++ src/components/server/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 const SUPABASE_URL = "https://buyfbpffwizllrfscsfi.supabase.co"
-const SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eWZicGZmd2l6bGxyZnNjc2ZpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NDQ4MTg5NSwiZXhwIjoxOTcwMDU3ODk1fQ.n4JELPwU-eHcEQ-otOGnThtyTMlCb1Gd_gYwedlHygo'
+const SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eWZicGZmd2l6bGxyZnNjc2ZpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NDQ4MjM4OSwiZXhwIjoxOTcwMDU4Mzg5fQ.rd0HeC2hlZpzSJNqfcBcbEMyvHcBlUnBXUPeickjBBQ'
 export const supabase = createClient(SUPABASE_URL, SERVICE_KEY);
 
